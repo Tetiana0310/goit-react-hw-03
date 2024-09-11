7d7ba0ec7681353e40613653a09c0cb98694abc0
Tetiana Posmashna <tetiana.posmashna@gmail.com>
2024-09-11 18:28:58 +0300

    Create structure

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,14 @@
+export default function ContactForm() {
+ return (<div>
+        <form>
+            <label >
+                <span>Name</span>
+                <input type="text" />
+            </label>
+         <label >
+              <span>Number</span>
+                <input type="number" />
+            </label>
+        </form>
+    </div>
+)}
